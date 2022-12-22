c33ea7394df2ae19b7bae7c6b86022ebbdc609f1
Nikita Gupta <nikitagupta1709@gmail.com>
2022-12-22 18:27:39 +0530

    model made

---
+++ server/models/users.model.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name : {type:String, default: ''},
+    email : {type:String, default:"", unique: true},
+    password: {type:String, default: ''},
+    profilePic : {type: String, default:''},
+    addedOn : {type: Number, default: Date.now()}
+});
+
+userSchema.method({
+    saveData: async function () {
+        return this.save();
+    }
+})
+
+userSchema.static({
+    findData: function(findObj){
+        return this.find(findObj);
+    },
+    findOneData: function(findObj){
+        return this.findOne(findObj);
+    },
+    findOneAndUpdateData: function(findObj, updateObj){
+        return this.findOneAndUpdate(findObj, updateObj,{
+            upsert: true,
+            new: true,
+            setDefaultsOnInsert: true
+        });
+    },
+})
+
+const userModel = mongoose.model("wc-user", userSchema)
+export default userModel;
